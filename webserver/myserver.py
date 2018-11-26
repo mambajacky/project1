4b04110a9f961acb74d6fe3ab711332687232eb8
mambajacky <34632995+mambajacky@users.noreply.github.com>
2018-11-26 00:17:04 -0500

    Update myserver.py

--- webserver/myserver.py
+++ webserver/myserver.py
@@ -294,10 +294,9 @@ def add():
   city = request.form['city']
   street_name = request.form['street_name']
   street_number = request.form['street_number']
-  cmd = 'INSERT INTO Users_Live(uid, uname, email_address, password, gender, state, city, street_name, street_number)  \
-                     VALUES (:uid), (:uname), (:email_address), (:password), (:gender), (:state), (:city), (:street_name), (:street_number)';
-  g.conn.execute(text(cmd), uid=uid, uname=uname, email_address=email_address, password=password, gender=gender, state=state,
-                 city=city, street_name=street_name, street_number=street_number);
+  cmd = "INSERT INTO Users_Live(uid, uname, email_address, password, gender, state, city, street_name, street_number)  \
+                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+  g.conn.execute(cmd, (uid, uname, email_address, password, gender, state, city, street_name, street_number))
   return redirect('/users')
 
 
